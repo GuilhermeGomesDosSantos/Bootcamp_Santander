438c136169ad4fb44a1c4ec25b3ad74e28fb03b5
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2025-11-22 17:00:23 -0300

    Operadores Aritmeticos

---
+++ Tipos_Operadores/Aritmeticos/Aritmeticos.py
@@ -0,0 +1,19 @@
+print("Adição:", 10 + 5)
+
+print('Subtração:', 50 - 10)
+
+print('Multiplicação:', 4 * 5)
+
+print('DIvisão Comum:', 20 / 4)
+
+print('Divisão Inteira:', 20 // 3)
+
+print('Modulo (Resto da Divisão):', 10 % 3)
+
+print('Exponenciação:', 2 ** 3)
+
+print(10 + 5 * 4)
+
+print((10 / 2) + (25 * 2) - (2 ** 2))
+
+print((10 / 2) + 25 * (2 - 2) ** 2)
