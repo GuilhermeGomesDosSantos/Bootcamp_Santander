df10e0f2b073256164ba23db7e8d35b91b1f0cd2
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2025-12-07 11:28:43 -0300

    Métodos úteis para Strings

---
+++ Metodos_Uteis_Strings/metodos_uteis.py
@@ -0,0 +1,19 @@
+curso = "pYtHon"
+curso_1 = "     Python  "
+curso_3 = "Python"
+
+print(curso.upper())
+
+print(curso.lower())
+
+print(curso.title())
+
+print(curso_1.strip())
+
+print(curso_1.lstrip())
+
+print(curso_1.rstrip())
+
+print(curso_3.center(10, "#"))
+
+print(".".join(curso_3))
