50f8dc478b691091729c903034a58abac0de4ac9
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2025-12-07 12:02:36 -0300

    Fatiamento de Strings

---
+++ Metodos_Uteis_Strings/fatiamento_string.py
@@ -0,0 +1,17 @@
+nome = "Guilherme Gomes dos Santos"
+
+print(nome[0])
+
+print(nome[-1])
+
+print(nome[:9])
+
+print(nome[10:])
+
+print(nome[10:15])
+
+print(nome[10:16:2])
+
+print(nome[:])
+
+print(nome[::-1])
